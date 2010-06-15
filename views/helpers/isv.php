6476fa7b74788e9aa10b7f13b6570bfc66d577c2
Dean Sofer <proloser@hotmail.com>
2010-06-15 13:38:38 -0700

    Fixed helper regex

--- views/helpers/isv.php
+++ views/helpers/isv.php
@@ -6,11 +6,14 @@ class IsvHelper extends AppHelper {
 		$replacement = '${1}1,$3';
 		preg_replace($pattern, $replacement, $content);*/
 
-		$content = str_replace(array('href="', 'src="'), array('href="' . $this->webroot, 'src="' . $this->webroot), $content);
+		//$content = str_replace(array('href="', 'src="'), array('href="' . $this->webroot, 'src="' . $this->webroot), $content);
 		
 		/*$pattern = array('/(href=")(?!http)/', '/(src=")(?!http)/');
 		$replace = array('href="/', 'src="/');
 		$content = preg_replace($pattern, $replace, $content);*/
+		
+		$pattern = '(?:href|src)="(?!/|http://)';
+		$content = preg_replace($pattern, '$1/',$content);
 
         return $this->output($content);
     }
